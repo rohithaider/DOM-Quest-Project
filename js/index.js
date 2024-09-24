343a6bbf7dea4d4ba4399a2fa10aa45ba5792f63
mohtashim haider <mohtashimhaider@mohtashims-MacBook-Air.local>
2024-09-25 00:06:02 +0600

    history section implemented

--- js/index.js
+++ js/index.js
@@ -38,24 +38,56 @@ const validationCheck = function(value){
 
 }
 
+const getCurrentDate = function(){
+    const currentDate = new Date();
+    const year = currentDate.getFullYear();          // 4-digit year (e.g., 2024)
+    const month = currentDate.getMonth() + 1;        // Month (0-11, so add 1)
+    const day = currentDate.getDate();               // Day of the month (1-31)
+    const hours = currentDate.getHours();            // Hours (0-23)
+    const minutes = currentDate.getMinutes();        // Minutes (0-59)
+    const seconds = currentDate.getSeconds(); 
+
+    const formattedTime =  `Date: ${month}-${day}-${year} ${hours}:${minutes}:${seconds} GMT +0600 (Bangladesh Standard Time)`;
+    return formattedTime;
+
+
+}
+
 
 
 //!Operation on card 1
 document.getElementById('card-1-btn').addEventListener('click',function(){
     const inputValue = parseFloat(getInputById('input-1'));
     const cardBalance = parseFloat(getTextValueById('card-balance'));
-    const mainBalance = parseFloat(getTextValueById('main-balance'))
-    console.log(cardBalance,mainBalance);
+    const mainBalance = parseFloat(getTextValueById('main-balance'));
+
+    //getting current date and time from reusable function
+    const currentDate = getCurrentDate();
+
+    
     if(mainBalance<inputValue){
         alert("Donation amount can not be greater than main balance");
     }else{
         if(validationCheck(inputValue)){
             const newCardBalance = cardBalance+inputValue
             const newBalance = mainBalance-inputValue;
+            
             document.getElementById('card-balance').innerText=newCardBalance;
             document.getElementById('main-balance').innerText=newBalance;
+            newHTML = `<div class="card lg:card-side bg-base-100 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">${inputValue} Taka is Donated for Flood at Noakhali, Bangladesh</h2>
+              <p>${currentDate}</p>
+            </div>
+          </div>`;
+          const historyContainer = document.getElementById('history-form');
+          historyContainer.innerHTML+=newHTML;
+
         
             my_modal.showModal();
+        }
+        else{
+            document.getElementById('input-1').value="";
         }
     }
     
@@ -63,13 +95,15 @@ document.getElementById('card-1-btn').addEventListener('click',function(){
     
 })
 
-
 //!Operations on card 2
 document.getElementById('card-2-btn').addEventListener('click',function(){
     const inputValue = parseFloat(getInputById('input-2'));
     const cardBalance = parseFloat(getTextValueById('card-balance-2'));
     const mainBalance = parseFloat(getTextValueById('main-balance'))
-    console.log(cardBalance,mainBalance);
+
+    //getting current date and time from reusable function
+    const currentDate = getCurrentDate();
+
     if(mainBalance<inputValue){
         alert("Donation amount can not be greater than main balance");
     }else{
@@ -78,8 +112,23 @@ document.getElementById('card-2-btn').addEventListener('click',function(){
             const newBalance = mainBalance-inputValue;
             document.getElementById('card-balance-2').innerText=newCardBalance;
             document.getElementById('main-balance').innerText=newBalance;
+
+            //adding to the history
+            newHTML = `<div class="card lg:card-side bg-base-100 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">${inputValue} Taka is Donated for Flood at Feni, Bangladesh</h2>
+              <p>${currentDate}</p>
+            </div>
+          </div>`;
+          const historyContainer = document.getElementById('history-form');
+          historyContainer.innerHTML+=newHTML;
+
+            
         
             my_modal.showModal();
+        }
+        else{
+            document.getElementById('input-2').value="";
         }
     }
 })
@@ -87,6 +136,9 @@ document.getElementById('card-2-btn').addEventListener('click',function(){
 document.getElementById('card-3-btn').addEventListener('click',function(){
     const inputValue = parseFloat(getInputById('input-3'));
     const cardBalance = parseFloat(getTextValueById('card-balance-3'));
+    //getting current date and time from reusable function
+    const currentDate = getCurrentDate();
+
     const mainBalance = parseFloat(getTextValueById('main-balance'))
     console.log(cardBalance,mainBalance);
     if(mainBalance<inputValue){
@@ -97,8 +149,41 @@ document.getElementById('card-3-btn').addEventListener('click',function(){
             const newBalance = mainBalance-inputValue;
             document.getElementById('card-balance-3').innerText=newCardBalance;
             document.getElementById('main-balance').innerText=newBalance;
+
+            //adding to the history
+            newHTML = `<div class="card lg:card-side bg-base-100 shadow-xl">
+            <div class="card-body">
+              <h2 class="card-title">${inputValue} Taka is Donated for Aid for the injured for the Quota Movement, Bangladesh</h2>
+              <p>${currentDate}</p>
+            </div>
+          </div>`;
+          const historyContainer = document.getElementById('history-form');
+          historyContainer.innerHTML+=newHTML;
         
             my_modal.showModal();
         }
+        else{
+            document.getElementById('input-3').value="";
+        }
     }
+})
+
+
+// !history button
+document.getElementById('history-btn').addEventListener('click',function(){
+    document.getElementById('donation-btn').classList.remove('bg-[#B4F461]');
+    document.getElementById('history-btn').classList.add('bg-[#B4F461]')
+    document.getElementById('donation-form').classList.add('hidden');
+    document.getElementById('history-form').classList.remove('hidden');
+
+    
+
+})
+
+//!donation button
+document.getElementById('donation-btn').addEventListener('click',function(){
+    document.getElementById('history-btn').classList.remove('bg-[#B4F461]');
+    document.getElementById('donation-btn').classList.add('bg-[#B4F461]');
+    document.getElementById('donation-form').classList.remove('hidden');
+    document.getElementById('history-form').classList.add('hidden');
 })
